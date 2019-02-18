803841ec50f2d91fa08ba132c2cacbd67eeff99b
nialditanawara <31877365+nialditanawara@users.noreply.github.com>
2019-02-19 01:21:07 +0700

    Add files via upload

---
+++ PBO2-10116058-Latihan2-Deklarasi/src/pbo2/pkg10116058/latihan2/deklarasi/PBO210116058Latihan2Deklarasi.java
@@ -0,0 +1,37 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10116058.latihan2.deklarasi;
+
+
+public class PBO210116058Latihan2Deklarasi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Deklarasi Variable
+        int nilaiInt;
+        final double PHI = 3.14; //konstanta uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        //Memberi nilai ke variable
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D'; 
+        
+        // Menampilkan Hasil
+        System.out.println();
+        System.out.println("Isi Variabel Nilai =" + nilaiInt);
+        System.out.println("Isi Variabel PHI =" + PHI);
+        System.out.println("Isi Variabel Logika =" + nilaiLogika);
+        System.out.println("Isi Variabel Karakter=" + nilaiKarakter);
+        
+     
+    }
+    
+}
